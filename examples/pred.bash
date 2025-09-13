701b9407d1d0facc5f8c06d302d582f3464f66bd
swgoo <swgoo91@gmail.com>
2025-09-13 05:14:13 +0000

    fix: update n_chunk_select and window_size parameters for improved model performance

--- examples/pred.bash
+++ examples/pred.bash
@@ -1,4 +1,2 @@
-python examples/squad.py predict configs/hmnet_3stage_XL_MW.yaml --ckpt-path ckpts/HMNet-squad-hmnet_3stage_XL_MW-epoch=10-val_loss=0.28.ckpt &&\
-python examples/squad.py predict configs/hmnet_3stage_XL_SW.yaml --ckpt-path ckpts/HMNet-squad-hmnet_3stage_XL_SW-epoch=10-val_loss=0.28.ckpt &&\
-python examples/squad.py predict configs/hmnet_3stage_XL_XXLW_XLS.yaml --ckpt-path ckpts/HMNet-squad-hmnet_3stage_XL_XXLW_XLS-epoch=18-val_loss=0.25.ckpt&&\
-python examples/squad.py predict configs/hnet_3stage_XL.yaml --ckpt-path ckpts/HNet-squad-hnet_3stage_XL-epoch=08-val_loss=0.20.ckpt --model-type HNet
+python examples/squad.py predict configs/hmnet_3stage_XL_LW.yaml --ckpt-path checkpoints/HMNet-squad-hmnet_3stage_XL_LW-epoch=15-val_loss=0.21.ckpt &&\
+python examples/squad.py train configs/hmnet_3stage_XL_MW.yaml --model-path ckpts/hmnet_3stage_XL_from_2stage_XL.pt
